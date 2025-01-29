c570234e94727fa84b3a62afb9b74edc1fdd584f
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-01-29 13:28:17 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,18 @@
+const accountId=122333;
+let accountEmail="keripalechinmay@gmail.com";
+var accountPass="123456";
+let accountCity="Jaysingpur";
+accountEmail="ck@gmail.com";
+accountPass="54321";
+accountCity="sangli";
+let accountState;
+
+// accountId=1; NOT ALLOWED
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+

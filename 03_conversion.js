cb3a35c436685349cbf7b3420cd787a1443cb530
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-02-06 23:59:17 +0530

    Add files via upload

---
+++ 03_conversion.js
@@ -0,0 +1,10 @@
+let value=3
+let negValue= -value
+console.log(negValue);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+2+3);
+console.log(1+2+"3");
+console.log(+true); // 1
+console.log(+"");

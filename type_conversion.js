d1e312963cc179015919a8560e43d365070a0084
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-05-01 12:50:45 +0530

    EXE 1

---
+++ type_conversion.js
@@ -0,0 +1,29 @@
+let num =undefined
+console.log(typeof num);
+console.log(typeof(num));
+
+let number="5"
+console.log(typeof number);
+console.log(typeof(number));
+
+let valueNumber = Number(num);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+// "33"=> 33
+// "33abc"=>NaN
+//true =>1
+//false =>0
+
+let a="abc"
+console.log(typeof a);
+let isBoolean=Boolean(a);
+console.log(isBoolean);
+
+let somenum=33;
+let strnum=String(somenum)
+console.log(strnum);
+console.log(typeof strnum);
+
+

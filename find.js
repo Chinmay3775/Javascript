05a78d40a8804fae37ea86e302a579f4416a11da
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-05-08 14:46:54 +0530

    FIND IN ARRAY

---
+++ find.js
@@ -0,0 +1,12 @@
+let iteams=["milk","bread","butter"];
+
+console.log(iteams.includes("bread"));
+
+console.log(iteams.indexOf("butter"));
+
+console.log("Cheese? "+iteams.includes("cheese"));
+
+iteams.push("cheese");
+console.log("Cheess? "+iteams.includes("cheese"));
+
+console.log("Index of MILK "+iteams.indexOf("milk"));

520abc6e2b0e6ccdb410a574ce40258246905a82
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-05-18 20:45:56 +0530

    While Loop In Js

---
+++ while_loop.js
@@ -0,0 +1,21 @@
+let i=1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+let fruits = ["apple", "banana", "cherry"];
+i = 0;
+while (i < fruits.length) {
+    console.log(fruits[i].toUpperCase());
+    i++;
+}
+
+// Exercise
+let j=1;
+let sum=0;
+while(j<=10){
+    sum+=j;
+    j++;
+}
+console.log(sum);
